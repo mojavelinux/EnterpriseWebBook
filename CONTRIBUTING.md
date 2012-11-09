0bff7b7a10b32a09fa6911a6e70f71a1c960ed39
Viktor Gamov <gamussa@gmail.com>
2012-11-09 15:55:42 -0500

    cleanup book content

--- CONTRIBUTING.md
+++
@@ -1,4 +0,0 @@
-## Contributing
----
-
-We are not going to accept and merge any pull requests till further notice
--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-#generate book
-asciidoc -d book -b html5 -a icons -a iconsdir=`brew --prefix asciidoc`/etc/asciidoc/images/icons -a theme=flask -a data-uri -a toc2 book.asciidoc
